e279f037229b0d309bc500d0cf899840e44a21fe
Stefan Damkjar <sdamkjar@ualberta.ca>
2015-03-03 17:50:48 -0700

    finished configuring main project

--- main/GNU ARM v4.8.3 Repo - Debug/emlib/em_assert.d
+++ main/GNU ARM v4.8.3 Repo - Debug/emlib/em_assert.d
@@ -1,4 +1,4 @@
 emlib/em_assert.o: ../emlib/em_assert.c \
- C:\SiliconLabs\SimplicityStudio\v2\developer\sdks\efm32\v2/emlib/inc/em_assert.h
+ C:\Users\sdamkjar\workspace\UALBERTA_ATHENA_CUBESATOBC/lib3rdParty/SiliconLabs/EFM32/emlib/inc/em_assert.h
 
-C:\SiliconLabs\SimplicityStudio\v2\developer\sdks\efm32\v2/emlib/inc/em_assert.h:
+C:\Users\sdamkjar\workspace\UALBERTA_ATHENA_CUBESATOBC/lib3rdParty/SiliconLabs/EFM32/emlib/inc/em_assert.h:
