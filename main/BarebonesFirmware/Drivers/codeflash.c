63563f0cf4bcc24b9d366876d1819de38e77303b
Stefan Damkjar <sdamkjar@ualberta.ca>
2015-03-03 17:25:29 -0700

    Cleaned up main for merge

--- main/BarebonesFirmware/Drivers/codeflash.c
+++
@@ -1,7 +0,0 @@
-/**
- * @file codeflash.c
- * @author Brandon Borden
- * @date 2015-02-21
- */
-
- #include "codeflash.h"
--- main/BarebonesFirmware/Drivers/tempsense.c
+++
@@ -1,7 +0,0 @@
-/**
- * @file tempsense.c
- * @author Brandon Borden
- * @date 2015-02-25
- */
-
- #include "tempsense.h"
