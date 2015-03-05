bde2b8f80aa69dcdb8aa0ab38f2bf879841a7b58
Stefan Damkjar <sdamkjar@ualberta.ca>
2015-03-04 21:40:22 -0700

    Messed everything up

--- main/GNU ARM v4.8.3 Repo - Debug/emlib/em_assert.d
+++
@@ -1,5 +0,0 @@
-emlib/em_assert.o: \
- C:/Users/sdamkjar/workspace/UALBERTA_ATHENA_CUBESATOBC/lib3rdParty/SiliconLabs/EFM32/emlib/src/em_assert.c \
- C:\Users\sdamkjar\workspace\UALBERTA_ATHENA_CUBESATOBC/lib3rdParty/SiliconLabs/EFM32/emlib/inc/em_assert.h
-
-C:\Users\sdamkjar\workspace\UALBERTA_ATHENA_CUBESATOBC/lib3rdParty/SiliconLabs/EFM32/emlib/inc/em_assert.h:
--- prototypes/EFM32GG280F1024_FreeRTOS_Blink/GNU ARM v4.8.3 Repo_Based - Debug/emlib/em_assert.d
+++
@@ -1,5 +0,0 @@
-emlib/em_assert.o: \
- C:/SiliconLabs/SimplicityStudio/v2/developer/sdks/efm32/v2/emlib/src/em_assert.c \
- C:\SiliconLabs\SimplicityStudio\v2\developer\sdks\efm32\v2/emlib/inc/em_assert.h
-
-C:\SiliconLabs\SimplicityStudio\v2\developer\sdks\efm32\v2/emlib/inc/em_assert.h:
