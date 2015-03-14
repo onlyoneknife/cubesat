a68bd94ce087a6df3e6aeb5f3addeab68a674abf
Brandon Borden <bborden@ualberta.ca>
2015-03-13 21:00:49 -0600

    Included gpio header file in gyro.c.  Fixed a prototype signature in gyro.h.

---
+++ libDrivers/inc/fram.h
@@ -0,0 +1,12 @@
+/**
+ * @file fram.h
+ * @author Brandon Borden
+ * @date 2015-03-13
+ */
+
+#ifndef FRAM_H_
+#define FRAM_H_
+
+
+
+#endif /* FRAM_H_ */
---
+++ libDrivers/src/fram.c
@@ -0,0 +1,6 @@
+/**
+ * @file fram.c
+ * @author Brandon Borden
+ * @date 2015-03-13
+ */
+
