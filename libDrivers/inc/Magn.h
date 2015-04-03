4c79d442967576c42aa95c21ab32807494573c11
Pzhang <pzhang@ualberta.ca>
2015-04-03 17:12:21 -0600

    New code for magnetometer

---
+++ libDrivers/inc/Magn.h
@@ -0,0 +1,23 @@
+/******************** (C) COPYRIGHT 2011 STMicroelectronics ********************
+* File Name          : lsm303dlhc_driver.h
+* Author             : MSH Application Team
+* Author             : Fabio Tota
+* Version            : $Revision:$
+* Date               : $Date:$
+* Description        : Descriptor Header for l3g4200d driver file
+*
+* HISTORY:
+* Date        | Modification                                | Author
+* 02/08/2011  | Initial Revision                            | Fabio Tota
+
+********************************************************************************
+* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*
+* THIS SOFTWARE IS SPECIFICALLY DESIGNED FOR EXCLUSIVE USE WITH ST PARTS.
+*
+*******************************************************************************/
