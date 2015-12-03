85666b426298c510f18ddf6a609eca97567b0f0e
Stefan Damkjar <sdamkjar@ualberta.ca>
2015-12-02 19:20:59 -0700

    Copied content generated by wscript for BoxSat

--- lib3rdParty/libcsp/include/csp/csp_autoconfig.h
+++ lib3rdParty/libcsp/include/csp/csp_autoconfig.h
@@ -1,25 +1,37 @@
-// TODO DELETE THIS FILE... It is just a test.
 
-#define GIT_REV "46d67426"
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
+#define _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
+
+#define GIT_REV "Vtimmy.002-13-g2579eb2"
 #define CSP_FREERTOS 1
-#undef CSP_POSIX
-#undef CSP_WINDOWS
-#undef CSP_MACOSX
+/* #undef CSP_POSIX */
+/* #undef CSP_WINDOWS */
+/* #undef CSP_MACOSX */
 #define CSP_DEBUG 1
-#undef CSP_USE_RDP
-#undef CSP_USE_CRC32
-#undef CSP_USE_HMAC
-#undef CSP_USE_XTEA
-#undef CSP_USE_PROMISC
-#undef CSP_USE_QOS
-#undef CSP_BUFFER_STATIC
-#define CSP_BUFFER_COUNT 12
-#define CSP_BUFFER_SIZE 320
+/* #undef CSP_VERBOSE */
+#define CSP_USE_RDP 1
+#define CSP_USE_CRC32 1
+#define CSP_USE_HMAC 1
+#define CSP_USE_XTEA 1
+/* #undef CSP_USE_PROMISC */
+#define CSP_USE_QOS 1
+/* #undef CSP_USE_DEDUP */
 #define CSP_CONN_MAX 10
 #define CSP_CONN_QUEUE_LENGTH 100
 #define CSP_FIFO_INPUT 10
 #define CSP_MAX_BIND_PORT 31
 #define CSP_RDP_MAX_WINDOW 20
 #define CSP_PADDING_BYTES 8
+#define CSP_TRANSACTION_SO 0
+#define CSP_LOG_LEVEL_DEBUG 1
+#define CSP_LOG_LEVEL_INFO 1
+#define CSP_LOG_LEVEL_WARN 1
+#define CSP_LOG_LEVEL_ERROR 1
 #define CSP_LITTLE_ENDIAN 1
-#undef CSP_BIG_ENDIAN
+/* #undef CSP_BIG_ENDIAN */
+#define CSP_HAVE_STDBOOL_H 1
+#define LIBCSP_VERSION "1.4"
+
+#endif /* _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF */
