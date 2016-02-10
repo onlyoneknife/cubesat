5aa122388ff09be4730a65c1dd2567197a418d25
sdamkjar <sdamkjar@ualberta.ca>
2016-02-10 14:52:23 -0700

    Added code to interface between hardware level and CSP level

--- lib3rdParty/libcsp/src/drivers/i2c/i2c_efm32.c
+++ lib3rdParty/libcsp/src/drivers/i2c/i2c_efm32.c
@@ -34,6 +34,53 @@ uint16_t RxBufferTail = 0;
  */
 int i2c_init(int handle, int mode, uint8_t addr, uint16_t speed, int queue_len_tx, int queue_len_rx, i2c_callback_t callback) {
 
+	/* Error checking */
+	if (handle >= efm32_device_count)
+		return E_NO_DEVICE;
+
+	if (device[handle].is_initialised)
+		return E_NO_ERR;
+
+	if (queue_len_tx <= 0)
+		return E_INVALID_PARAM;
+
+	if (mode != I2C_MASTER) {
+		driver_debug(DEBUG_I2C, "driver does not support slave mode operation\r\n");
+		return E_INVALID_PARAM;
+	}
+
+	if (i2c_lock == NULL)
+		i2c_lock = xSemaphoreCreateMutex();
+	if (i2c_lock == NULL)
+		return E_NO_BUFFER;
+
+	/* TX Queue */
+	if (device[handle].tx.queue == NULL) {
+		device[handle].tx.queue = xQueueCreate(queue_len_tx, sizeof(i2c_frame_t *));
+	}
+
+	/* RX Queue */
+	if ((device[handle].rx.queue == NULL) && (queue_len_rx > 0)) {
+		device[handle].rx.queue = xQueueCreate(queue_len_rx, sizeof(i2c_frame_t *));
+	}
+
+	/* Callback */
+	if (callback != NULL) {
+		device[handle].callback = callback;
+	}
+
+	/* Call board specific ISR initialization */
+	pca9665_isr_init();
+
+	/* Initialise device */
+	int result = efm32_init(handle, device[handle].base, addr, speed);
+	if (result == E_NO_ERR) {
+		device[handle].is_initialised = 1;
+	} else {
+		driver_debug(DEBUG_I2C, "Failed to initialise efm32 driver\r\n");
+	}
+
+	return result;
 
 }
 
@@ -47,6 +94,25 @@ int i2c_init(int handle, int mode, uint8_t addr, uint16_t speed, int queue_len_t
  */
 int i2c_send(int handle, i2c_frame_t * frame, uint16_t timeout) {
 
+	if (handle >= efm32_device_count)
+		return E_NO_DEVICE;
+
+	if (!device[handle].is_initialised)
+		return E_NO_DEVICE;
+
+	if (xQueueSendToBack(device[handle].tx.queue, &frame, timeout) == pdFALSE)
+		return E_TIMEOUT;
+
+	/* Check state in critical region */
+	vPortEnterCritical();
+	{
+		/* If not currently busy, send the start condition... */
+		if (device[handle].is_busy == 0)
+			// TODO Add call to I2C driver
+	}
+	vPortExitCritical();
+
+	return E_NO_ERR;
 
 }
 
@@ -61,5 +127,18 @@ int i2c_send(int handle, i2c_frame_t * frame, uint16_t timeout) {
  */
 int i2c_receive(int handle, i2c_frame_t ** frame, uint16_t timeout) {
 
+	if (handle >= efm32_device_count)
+		return E_NO_DEVICE;
+
+	if (!device[handle].is_initialised)
+		return E_NO_DEVICE;
+
+	if (device[handle].rx.queue == NULL)
+		return E_NO_DEVICE;
+
+	if (xQueueReceive(device[handle].rx.queue, frame, timeout) == pdFALSE)
+		return E_TIMEOUT;
+
+	return E_NO_ERR;
 
 }
