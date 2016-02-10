bdcfb2c5aff5fab4fb6a665cf443fd99a2f9c12d
sdamkjar <sdamkjar@ualberta.ca>
2016-02-10 14:50:58 -0700

    Added error codes to sharedtypes

--- libDrivers/inc/sharedtypes.h
+++ libDrivers/inc/sharedtypes.h
@@ -30,6 +30,28 @@ typedef struct {
 	int16_t AXIS_Z;
 } AxesRaw_t;
 
+/* --Error Codes -------------------------------------------------------------*/
+enum
+{
+	E_NO_ERR
+	E_NO_DEVICE
+	E_MALLOC_FAIL
+	E_THREAD_FAIL
+	E_NO_QUEUE
+	E_INVALID_BUF_SIZE
+	E_INVALID_PARAM
+	E_NO_SS
+	E_GARBLED_BUFFER
+	E_FLASH_ERROR
+	E_BOOT_SER
+	E_BOOT_DEBUG
+	E_BOOT_FLASH
+	E_TIMEOUT
+	E_NO_BUFFER
+	E_OUT_OF_MEM
+	E_FAIL
+};
+
 #endif /*__SHARED__TYPES*/
 
 #endif /* SHAREDTYPES_H_ */
