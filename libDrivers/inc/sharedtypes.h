fdde89132014ce66e01caf0cb900d9856474845d
sdamkjar <sdamkjar@ualberta.ca>
2016-05-19 12:58:17 -0600

    added gomspace error codes to shared types header

--- libDrivers/inc/sharedtypes.h
+++ libDrivers/inc/sharedtypes.h
@@ -26,6 +26,27 @@ typedef struct {
 	int16_t AXIS_Z;
 } AxesRaw_t;
 
+
+
+
+#define E_NO_ERR 			-1
+#define E_NO_DEVICE 		-2
+#define E_MALLOC_FAIL 		-3
+#define E_THREAD_FAIL 		-4
+#define E_NO_QUEUE 			-5
+#define E_INVALID_BUF_SIZE 	-6
+#define E_INVALID_PARAM 	-7
+#define E_NO_SS 			-8
+#define E_GARBLED_BUFFER 	-9
+#define E_FLASH_ERROR 		-10
+#define E_BOOT_SER 			-13
+#define E_BOOT_DEBUG 		-14
+#define E_BOOT_FLASH 		-15
+#define E_TIMEOUT 			-16
+#define E_NO_BUFFER 		-17
+#define E_OUT_OF_MEM 		-18
+#define E_FAIL 				-19
+
 #endif /*__SHARED__TYPES*/
 
 #endif /* SHAREDTYPES_H_ */
